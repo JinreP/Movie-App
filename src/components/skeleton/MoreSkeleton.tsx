9baef65b383ab6dbf375d870041fbdf7115f6a3f
Subee <YOUR_GITHUB_EMAIL@example.com>
2025-10-16 08:06:38 +0800

    sss

--- src/components/skeleton/MoreSkeleton.tsx
+++ src/components/skeleton/MoreSkeleton.tsx
@@ -6,10 +6,6 @@ export function MoreSkeleton({ count = 10 }: { count: number }) {
       {Array.from({ length: count }).map((_, i) => (
         <div key={i} className="w-[230px]">
           <Skeleton className="h-[340px] w-full rounded-md" />
-          <div className="mt-3 space-y-2">
-            <Skeleton className="h-4 w-3/4" />
-            <Skeleton className="h-4 w-1/3" />
-          </div>
         </div>
       ))}
     </div>
