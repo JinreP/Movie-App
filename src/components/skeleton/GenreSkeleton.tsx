4105fb3d1dbae98be3fa30069c79ec5a3727b790
Subee <YOUR_GITHUB_EMAIL@example.com>
2025-12-08 22:21:55 +0800

    skeleton genre fixed

--- src/components/skeleton/GenreSkeleton.tsx
+++ src/components/skeleton/GenreSkeleton.tsx
@@ -1,21 +1,37 @@
 import { Skeleton } from "../ui/skeleton";
 
-export function GenreSkeleton({ count = 5 }: { count: number }) {
+export function GenreSkeleton({ count = 12 }: { count: number }) {
   return (
-    <div className="w-full flex flex-wrap gap-10 mt-10 justify-center">
-      <div className="flex flex-wrap w-[1400px]">
-        {Array.from({ length: count }).map((_, i) => (
-          <div key={i} className="w-[60px]">
-            <Skeleton className="h-[30px] w-full rounded-md" />
+    <div className="flex justify-center  mt-10">
+      <div className="flex w-full  gap-20">
+        <div className="w-[260px]">
+          <Skeleton className="h-10 w-32 mb-2" />
+          <Skeleton className="h-6 w-48 mb-6" />
+
+          <div className="flex flex-wrap gap-5 mt-4 w-[400px]">
+            {Array.from({ length: 23 }).map((_, i) => (
+              <div key={i} className="flex items-center gap-2">
+                <Skeleton className="h-6 w-24" />
+                <Skeleton className="h-6 w-10 rounded-full" />{" "}
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      {Array.from({ length: count }).map((_, i) => (
-        <div key={i} className="w-[230px]">
-          <Skeleton className="h-[340px] w-full rounded-md" />
         </div>
-      ))}
-      <Skeleton className="w-[350px] h-[50px] ml-240" />
+
+        <div className="flex flex-col w-[1000px]">
+          <Skeleton className="h-8 w-[260px] mb-6" />
+
+          <div className="flex flex-wrap gap-6">
+            {Array.from({ length: count }).map((_, i) => (
+              <div key={i} className="w-[230px]">
+                <Skeleton className="h-[340px] rounded-md" />
+              </div>
+            ))}
+          </div>
+
+          <Skeleton className="w-[350px] h-[50px] mt-10" />
+        </div>
+      </div>
     </div>
   );
 }
